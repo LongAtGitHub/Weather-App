5248a71d78d2eaeb41edc57c6d937fc8efdf5fdd
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2022-12-19 15:30:02 -0600

    Working well

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// /**
-//  * Fetch the API
-//  * Put the info into the var data
-//  */
-// async function getWeather() {
-//     const apiKey = '75e14ed0cc1e9172da2f18160c44f735';
-//     const city = 'London';
-//     const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-//     try
-//     {
-//         const response = await fetch(apiUrl);
-//         const data = await response.json();
-//         console.log(data);
-//     } 
-//     catch (error) 
-//     {
-//         console.log("There are some errors with the API - Long");
-//         console.log(error);
-//     }
-// }  
+// main script
+main();
 
+/**
+ * main function
+ */
+async function main()
+{
+    const [lat, long] = await getLocation(); 
+    const data = await getWeather(lat, long);
+    
+    console.log(data.weather.id);
+    document.getElementById("country").innerHTML += data.sys.country;
+    document.getElementById("city").innerHTML += data.name;
+    document.getElementById("temperature").innerHTML += data.main.temp;
+    document.getElementById("weatherDesc").innerHTML += data.weather[0].description;
+    
+}
+
+/**
+ * @returns latitude and longitude in the form of tuple
+ */
 async function getLocation() {
     try {
       const position = await new Promise((resolve, reject) => {
@@ -30,18 +31,34 @@ async function getLocation() {
     }
     return [null, null];
   }
-  
+
 /**
- * main function
+ * Get the info of a place given the lat and long using await
+ * @param lat latitude of the location
+ * @param long longitude of the location
+ * @param units metrics or imperial, if left unfilled it would be metric
+ * @param return the data json object containing info
  */
-async function main()
-{
-    [lat, long] = await getLocation();
-    console.log(long);
-    console.log(lat);
+async function getWeather(lat, lon, units = "metric") {
+    const apiKey = "75e14ed0cc1e9172da2f18160c44f735";
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();        
+        return data;
+    }
+    
+    catch (error) {
+        console.log("There are some errors with the API - Long");
+        console.log(error);
+        return null;
+    }
+    
 }
 
-main();
+  
+
+
 
   
   
