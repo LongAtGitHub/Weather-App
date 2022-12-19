b79475c6c4bd5c8c26d7ca4d86b8993ec2ec8660
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2022-12-19 12:08:30 -0600

    Capture the variables long and lat

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-/**
- * Fetch the API
- * Put the info into the var data
- */
-async function getWeather() {
-    const apiKey = '75e14ed0cc1e9172da2f18160c44f735';
-    const city = 'London';
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    try
-    {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } 
-    catch (error) 
-    {
-        console.log("There are some errors with the API - Long");
-        console.log(error);
-    }
-}  
+// /**
+//  * Fetch the API
+//  * Put the info into the var data
+//  */
+// async function getWeather() {
+//     const apiKey = '75e14ed0cc1e9172da2f18160c44f735';
+//     const city = 'London';
+//     const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//     try
+//     {
+//         const response = await fetch(apiUrl);
+//         const data = await response.json();
+//         console.log(data);
+//     } 
+//     catch (error) 
+//     {
+//         console.log("There are some errors with the API - Long");
+//         console.log(error);
+//     }
+// }  
 
+async function getLocation() {
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      return [position.coords.latitude, position.coords.longitude];
+    } catch (error) {
+      console.log(error);
+    }
+    return [null, null];
+  }
+  
 /**
- * Update weather
- * Modify the content of the html
- * @param data the weather data fetched from the API
+ * main function
  */
-function updateWeather(data) {
-    const elementList = document.getElementsByTagName("p");
-    elementList[0].innerHTML("City: ")
+async function main()
+{
+    [lat, long] = await getLocation();
+    console.log(long);
+    console.log(lat);
 }
 
-// main call
-getWeather();
+main();
+
+  
   
 
